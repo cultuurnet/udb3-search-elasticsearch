d573465b4ac4d576958bb675df1352d6deb62ac2
Bert Ramakers <bertramakers@me.com>
2017-02-17 19:35:18 +0100

    III-1779: Add search service for offers.

---
+++ src/Offer/ElasticSearchOfferSearchService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace CultuurNet\UDB3\Search\ElasticSearch\Offer;
+
+use CultuurNet\UDB3\Search\ElasticSearch\ElasticSearchPagedResultSetFactoryInterface;
+use CultuurNet\UDB3\Search\ElasticSearch\HasElasticSearchClient;
+use CultuurNet\UDB3\Search\Offer\OfferSearchParameters;
+use CultuurNet\UDB3\Search\Offer\OfferSearchServiceInterface;
+use CultuurNet\UDB3\Search\PagedResultSet;
+use Elasticsearch\Client;
+use ValueObjects\StringLiteral\StringLiteral;
+
+class ElasticSearchOfferSearchService implements OfferSearchServiceInterface
+{
+    use HasElasticSearchClient;
+
+    /**
+     * @var ElasticSearchPagedResultSetFactoryInterface
+     */
+    private $pagedResultSetFactory;
+
+    /**
+     * @param Client $elasticSearchClient
+     * @param StringLiteral $indexName
+     * @param StringLiteral $documentType
+     * @param ElasticSearchPagedResultSetFactoryInterface $pagedResultSetFactory
+     */
+    public function __construct(
+        Client $elasticSearchClient,
+        StringLiteral $indexName,
+        StringLiteral $documentType,
+        ElasticSearchPagedResultSetFactoryInterface $pagedResultSetFactory
+    ) {
+        $this->elasticSearchClient = $elasticSearchClient;
+        $this->indexName = $indexName;
+        $this->documentType = $documentType;
+        $this->pagedResultSetFactory = $pagedResultSetFactory;
+    }
+
+    /**
+     * @param OfferSearchParameters $searchParameters
+     * @return PagedResultSet
+     */
+    public function search(OfferSearchParameters $searchParameters)
+    {
+        $query = ElasticSearchOfferQuery::fromSearchParameters($searchParameters);
+
+        $response = $this->elasticSearchClient->search(
+            $this->createParameters(['body' => $query->toArray()])
+        );
+
+        return $this->pagedResultSetFactory->createPagedResultSet(
+            $searchParameters->getLimit(),
+            $response
+        );
+    }
+}
