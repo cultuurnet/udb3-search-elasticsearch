00f7c016d867569aac043210eb4d3a45a28c8122
Bert Ramakers <bertramakers@me.com>
2017-02-17 20:13:20 +0100

    III-1779: Add json transformer that returns a document with only @id and @type.

---
+++ src/JsonDocument/ResultSetJsonDocumentTransformer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CultuurNet\UDB3\Search\ElasticSearch\JsonDocument;
+
+use CultuurNet\UDB3\ReadModel\JsonDocument;
+use CultuurNet\UDB3\Search\JsonDocument\JsonDocumentTransformerInterface;
+
+/**
+ * Converts Event, Place and Organizer results to minimal documents that only
+ * contain @id and @type.
+ * Should be used when returning search results.
+ */
+class ResultSetJsonDocumentTransformer implements JsonDocumentTransformerInterface
+{
+    public function transform(JsonDocument $jsonDocument)
+    {
+        $body = $jsonDocument->getBody();
+
+        $newBody = (object) [
+            '@id' => $body->{'@id'},
+            '@type' => $body->{'@type'},
+        ];
+
+        return $jsonDocument->withBody($newBody);
+    }
+}
