4c15e3caa4a3d9f08e5b1d0d6976dbd4c8a98811
Bert Ramakers <bertramakers@me.com>
2017-02-20 17:32:28 +0100

    III-1779: Add response validator for the paged result set factory.

--- src/ElasticSearchPagedResultSetFactory.php
+++ src/ElasticSearchPagedResultSetFactory.php
@@ -3,16 +3,38 @@
 namespace CultuurNet\UDB3\Search\ElasticSearch;
 
 use CultuurNet\UDB3\ReadModel\JsonDocument;
+use CultuurNet\UDB3\Search\ElasticSearch\Validation\ElasticSearchResponseValidatorInterface;
+use CultuurNet\UDB3\Search\ElasticSearch\Validation\PagedResultSetResponseValidator;
 use CultuurNet\UDB3\Search\PagedResultSet;
 use ValueObjects\Number\Natural;
 
 class ElasticSearchPagedResultSetFactory implements ElasticSearchPagedResultSetFactoryInterface
 {
     /**
+     * @var ElasticSearchResponseValidatorInterface|null
+     */
+    private $responseValidator;
+
+    /**
+     * @param ElasticSearchResponseValidatorInterface|null $responseValidator
+     */
+    public function __construct(
+        ElasticSearchResponseValidatorInterface $responseValidator = null
+    ) {
+        if (is_null($responseValidator)) {
+            $responseValidator = new PagedResultSetResponseValidator();
+        }
+
+        $this->responseValidator = $responseValidator;
+    }
+
+    /**
      * @inheritdoc
      */
     public function createPagedResultSet(Natural $perPage, array $response)
     {
+        $this->responseValidator->validate($response);
+
         $total = new Natural($response['hits']['total']);
 
         $results = array_map(
