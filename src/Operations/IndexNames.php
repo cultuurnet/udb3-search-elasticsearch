00ea54c32a2af2b268e2ad9fa12a733ed34cd212
Bert Ramakers <bertramakers@me.com>
2017-02-27 17:05:31 +0100

    III-1937: Use different read and write aliases for indexes to make migrations easier.

--- src/Operations/IndexNames.php
+++ src/Operations/IndexNames.php
@@ -4,11 +4,13 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 class IndexNames
 {
-    const UDB3_CORE_ALIAS = 'udb3_core';
+    const UDB3_CORE_READ_ALIAS = 'udb3_core_read';
+    const UDB3_CORE_WRITE_ALIAS = 'udb3_core_write';
     const UDB3_CORE_LATEST = 'udb3_core_v1';
     const UDB3_CORE_PREVIOUS = 'udb3_core_20161011000440';
 
-    const GEOSHAPES_ALIAS = 'geoshapes';
+    const GEOSHAPES_READ_ALIAS = 'geoshapes_read';
+    const GEOSHAPES_WRITE_ALIAS = 'geoshapes_write';
     const GEOSHAPES_LATEST = 'geoshapes_v1';
     const GEOSHAPES_PREVIOUS = null;
 }
