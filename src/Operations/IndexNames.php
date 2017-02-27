4f27c9de566348c834e16e4dfd78cd18ec1d3c64
Bert Ramakers <bertramakers@me.com>
2017-02-27 15:45:13 +0100

    III-1937: Add constants with latest index names and aliases.

---
+++ src/Operations/IndexNames.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
+
+class IndexNames
+{
+    const UDB3_CORE_ALIAS = 'udb3_core';
+    const UDB3_CORE_LATEST = 'udb3_core_v1';
+    const UDB3_CORE_PREVIOUS = 'udb3_core_20161011000440';
+
+    const GEOSHAPES_ALIAS = 'geoshapes';
+    const GEOSHAPES_LATEST = 'geoshapes_v1';
+    const GEOSHAPES_PREVIOUS = null;
+}
