2972dd6b272a4b36ccff0378148a0d85c955afa3
Bert Ramakers <bertramakers@me.com>
2017-03-08 11:44:40 +0100

    III-1958: Update EventJsonDocumentTransformer to include free text search properties

--- src/Event/EventJsonDocumentTransformer.php
+++ src/Event/EventJsonDocumentTransformer.php
@@ -2,7 +2,9 @@
 
 namespace CultuurNet\UDB3\Search\ElasticSearch\Event;
 
+use CultuurNet\UDB3\Language;
 use CultuurNet\UDB3\ReadModel\JsonDocument;
+use CultuurNet\UDB3\Search\ElasticSearch\IdUrlParserInterface;
 use CultuurNet\UDB3\Search\JsonDocument\JsonDocumentTransformerInterface;
 
 /**
@@ -11,13 +13,65 @@ use CultuurNet\UDB3\Search\JsonDocument\JsonDocumentTransformerInterface;
  */
 class EventJsonDocumentTransformer implements JsonDocumentTransformerInterface
 {
+    /**
+     * @var IdUrlParserInterface
+     */
+    private $idUrlParser;
+
+    /**
+     * @var Language
+     */
+    private $defaultLanguageCode;
+
+    /**
+     * @param IdUrlParserInterface $idUrlParser
+     * @param Language $defaultLanguageCode
+     */
+    public function __construct(
+        IdUrlParserInterface $idUrlParser,
+        Language $defaultLanguageCode
+    ) {
+        $this->idUrlParser = $idUrlParser;
+        $this->defaultLanguageCode = $defaultLanguageCode;
+    }
+
+    /**
+     * @param JsonDocument $jsonDocument
+     * @return JsonDocument
+     */
     public function transform(JsonDocument $jsonDocument)
     {
+        $lang = $this->defaultLanguageCode->getCode();
+
         $body = $jsonDocument->getBody();
         $newBody = new \stdClass();
 
-        $newBody->{"@id"} = $body->{"@id"};
-        $newBody->{"@type"} = 'Event';
+        $this->copyIdentifiers($body, $newBody, 'Event');
+
+        $newBody->name = $body->name->{$lang};
+
+        if (isset($body->description->{$lang})) {
+            $newBody->description = $body->description->{$lang};
+        }
+
+        $this->copyLabels($body, $newBody);
+
+        if (isset($body->terms)) {
+            $newBody->terms = $body->terms;
+        }
+
+        if (isset($body->performer)) {
+            $newBody->performer = array_map(
+                function ($performer) {
+                    return $performer->name;
+                },
+                $body->performer
+            );
+        }
+
+        $newBody->addressLocality = $body->location->address->addressLocality;
+        $newBody->postalCode = $body->location->address->postalCode;
+        $newBody->streetAddress = $body->location->address->streetAddress;
 
         if (isset($body->location->geo)) {
             $newBody->geo = new \stdClass();
@@ -33,6 +87,62 @@ class EventJsonDocumentTransformer implements JsonDocumentTransformerInterface
             ];
         }
 
+        $newBody->location = new \stdClass();
+        $this->copyIdentifiers($body->location, $newBody->location, 'Place');
+        $newBody->location->name = $body->location->name->{$lang};
+        $this->copyLabels($body->location, $newBody->location);
+
+        if (isset($body->organizer)) {
+            $newBody->organizer = new \stdClass();
+            $this->copyIdentifiers($body->organizer, $newBody->organizer, 'Organizer');
+            $newBody->organizer->name = $body->organizer->name;
+            $this->copyLabels($body->organizer, $newBody->organizer);
+        }
+
         return $jsonDocument->withBody($newBody);
+    }
+
+    /**
+     * @param \stdClass $from
+     * @param \stdClass $to
+     * @param $fallbackType
+     */
+    private function copyIdentifiers(\stdClass $from, \stdClass $to, $fallbackType)
+    {
+        $to->{"@id"} = $from->{"@id"};
+        $to->{"@type"} = isset($from->{"@type"}) ? $from->{"@type"} : $fallbackType;
+        $to->id = $this->idUrlParser->getIdFromUrl($from->{"@id"});
+    }
+
+    /**
+     * @param \stdClass $from
+     * @param \stdClass $to
+     */
+    private function copyLabels(\stdClass $from, \stdClass $to)
+    {
+        $labels = $this->getLabels($from);
+
+        if (!empty($labels)) {
+            $to->labels = $labels;
+        }
+    }
+
+    /**
+     * @param \stdClass $object
+     * @return array
+     */
+    private function getLabels(\stdClass $object)
+    {
+        $labels = [];
+
+        if (isset($object->labels)) {
+            $labels = array_merge($labels, $object->labels);
+        }
+
+        if (isset($object->hiddenLabels)) {
+            $labels = array_merge($labels, $object->hiddenLabels);
+        }
+
+        return $labels;
     }
 }
--- tests/Event/EventJsonDocumentTransformerTest.php
+++ tests/Event/EventJsonDocumentTransformerTest.php
@@ -2,7 +2,9 @@
 
 namespace CultuurNet\UDB3\Search\ElasticSearch\Event;
 
+use CultuurNet\UDB3\Language;
 use CultuurNet\UDB3\ReadModel\JsonDocument;
+use CultuurNet\UDB3\Search\ElasticSearch\PathEndIdUrlParser;
 use CultuurNet\UDB3\Search\JsonDocument\Testing\AssertJsonDocumentTrait;
 
 class EventJsonDocumentTransformerTest extends \PHPUnit_Framework_TestCase
@@ -16,18 +18,37 @@ class EventJsonDocumentTransformerTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->transformer = new EventJsonDocumentTransformer();
+        $this->transformer = new EventJsonDocumentTransformer(
+            new PathEndIdUrlParser(),
+            new Language('nl')
+        );
     }
 
     /**
      * @test
      */
-    public function it_transforms_location_geocoordinates_to_geojson_coordinates_on_the_event_itself()
+    public function it_transforms_required_fields()
     {
-        $original = file_get_contents(__DIR__ . '/data/original-with-geocoordinates.json');
+        $original = file_get_contents(__DIR__ . '/data/original.json');
         $originalDocument = new JsonDocument('23017cb7-e515-47b4-87c4-780735acc942', $original);
 
-        $expected = file_get_contents(__DIR__ . '/data/indexed-with-geocoordinates.json');
+        $expected = file_get_contents(__DIR__ . '/data/indexed.json');
+        $expectedDocument = new JsonDocument('23017cb7-e515-47b4-87c4-780735acc942', $expected);
+
+        $actualDocument = $this->transformer->transform($originalDocument);
+
+        $this->assertJsonDocumentEquals($this, $expectedDocument, $actualDocument);
+    }
+
+    /**
+     * @test
+     */
+    public function it_transforms_optional_fields_if_present()
+    {
+        $original = file_get_contents(__DIR__ . '/data/original-with-optional-fields.json');
+        $originalDocument = new JsonDocument('23017cb7-e515-47b4-87c4-780735acc942', $original);
+
+        $expected = file_get_contents(__DIR__ . '/data/indexed-with-optional-fields.json');
         $expectedDocument = new JsonDocument('23017cb7-e515-47b4-87c4-780735acc942', $expected);
 
         $actualDocument = $this->transformer->transform($originalDocument);
