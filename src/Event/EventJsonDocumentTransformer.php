e167a4de42d3176375d1f52f9f039a450ba72345
Bert Ramakers <bertramakers@me.com>
2017-03-08 12:28:13 +0100

    III-1958: Rework internals of Event transformer to make some parts more re-usable

--- src/Event/EventJsonDocumentTransformer.php
+++ src/Event/EventJsonDocumentTransformer.php
@@ -41,63 +41,23 @@ class EventJsonDocumentTransformer implements JsonDocumentTransformerInterface
      */
     public function transform(JsonDocument $jsonDocument)
     {
-        $lang = $this->defaultLanguageCode->getCode();
-
         $body = $jsonDocument->getBody();
         $newBody = new \stdClass();
 
         $this->copyIdentifiers($body, $newBody, 'Event');
 
-        $newBody->name = $body->name->{$lang};
-
-        if (isset($body->description->{$lang})) {
-            $newBody->description = $body->description->{$lang};
-        }
+        $this->copyName($body, $newBody, $this->defaultLanguageCode);
+        $this->copyDescription($body, $newBody, $this->defaultLanguageCode);
 
         $this->copyLabels($body, $newBody);
+        $this->copyTerms($body, $newBody);
 
-        if (isset($body->terms)) {
-            $newBody->terms = $body->terms;
-        }
+        $this->copyPerformer($body, $newBody);
 
-        if (isset($body->performer)) {
-            $newBody->performer = array_map(
-                function ($performer) {
-                    return $performer->name;
-                },
-                $body->performer
-            );
-        }
+        $this->copyAddressAndGeoInformation($body->location, $newBody);
 
-        $newBody->addressLocality = $body->location->address->addressLocality;
-        $newBody->postalCode = $body->location->address->postalCode;
-        $newBody->streetAddress = $body->location->address->streetAddress;
-
-        if (isset($body->location->geo)) {
-            $newBody->geo = new \stdClass();
-            $newBody->geo->type = 'Point';
-
-            // Important! In GeoJSON, and therefore Elasticsearch, the correct coordinate order is longitude, latitude
-            // (X, Y) within coordinate arrays. This differs from many Geospatial APIs (e.g., Google Maps) that
-            // generally use the colloquial latitude, longitude (Y, X).
-            // @see https://www.elastic.co/guide/en/elasticsearch/reference/current/geo-shape.html#input-structure
-            $newBody->geo->coordinates = [
-                $body->location->geo->longitude,
-                $body->location->geo->latitude,
-            ];
-        }
-
-        $newBody->location = new \stdClass();
-        $this->copyIdentifiers($body->location, $newBody->location, 'Place');
-        $newBody->location->name = $body->location->name->{$lang};
-        $this->copyLabels($body->location, $newBody->location);
-
-        if (isset($body->organizer)) {
-            $newBody->organizer = new \stdClass();
-            $this->copyIdentifiers($body->organizer, $newBody->organizer, 'Organizer');
-            $newBody->organizer->name = $body->organizer->name;
-            $this->copyLabels($body->organizer, $newBody->organizer);
-        }
+        $this->copyLocation($body, $newBody, $this->defaultLanguageCode);
+        $this->copyOrganizer($body, $newBody);
 
         return $jsonDocument->withBody($newBody);
     }
@@ -112,6 +72,31 @@ class EventJsonDocumentTransformer implements JsonDocumentTransformerInterface
         $to->{"@id"} = $from->{"@id"};
         $to->{"@type"} = isset($from->{"@type"}) ? $from->{"@type"} : $fallbackType;
         $to->id = $this->idUrlParser->getIdFromUrl($from->{"@id"});
+    }
+
+    /**
+     * @param \stdClass $from
+     * @param \stdClass $to
+     * @param Language $language
+     */
+    private function copyName(\stdClass $from, \stdClass $to, Language $language)
+    {
+        $lang = $language->getCode();
+        $to->name = $from->name->{$lang};
+    }
+
+    /**
+     * @param \stdClass $from
+     * @param \stdClass $to
+     * @param Language $language
+     */
+    private function copyDescription(\stdClass $from, \stdClass $to, Language $language)
+    {
+        $lang = $language->getCode();
+
+        if (isset($from->description->{$lang})) {
+            $to->description = $from->description->{$lang};
+        }
     }
 
     /**
@@ -144,5 +129,92 @@ class EventJsonDocumentTransformer implements JsonDocumentTransformerInterface
         }
 
         return $labels;
+    }
+
+    /**
+     * @param \stdClass $from
+     * @param \stdClass $to
+     */
+    private function copyTerms(\stdClass $from, \stdClass $to)
+    {
+        if (isset($from->terms)) {
+            $to->terms = $from->terms;
+        }
+    }
+
+    /**
+     * @param \stdClass $from
+     * @param \stdClass $to
+     */
+    private function copyPerformer(\stdClass $from, \stdClass $to)
+    {
+        if (isset($from->performer) && is_array($from->performer)) {
+            $to->performer = array_map(
+                function ($performer) {
+                    return $performer->name;
+                },
+                $from->performer
+            );
+        }
+    }
+
+    /**
+     * @param \stdClass $from
+     * @param \stdClass $to
+     */
+    private function copyAddressAndGeoInformation(\stdClass $from, \stdClass $to)
+    {
+        $to->addressLocality = $from->address->addressLocality;
+        $to->postalCode = $from->address->postalCode;
+        $to->streetAddress = $from->address->streetAddress;
+
+        if (isset($from->geo)) {
+            $to->geo = new \stdClass();
+            $to->geo->type = 'Point';
+
+            // Important! In GeoJSON, and therefore Elasticsearch, the correct coordinate order is longitude, latitude
+            // (X, Y) within coordinate arrays. This differs from many Geospatial APIs (e.g., Google Maps) that
+            // generally use the colloquial latitude, longitude (Y, X).
+            // @see https://www.elastic.co/guide/en/elasticsearch/reference/current/geo-shape.html#input-structure
+            $to->geo->coordinates = [
+                $from->geo->longitude,
+                $from->geo->latitude,
+            ];
+        }
+    }
+
+    /**
+     * @param \stdClass $from
+     * @param \stdClass $to
+     * @param Language $language
+     */
+    private function copyLocation(\stdClass $from, \stdClass $to, Language $language)
+    {
+        if (!isset($to->location)) {
+            $to->location = new \stdClass();
+        }
+
+        $this->copyIdentifiers($from->location, $to->location, 'Place');
+        $this->copyName($from->location, $to->location, $language);
+        $this->copyLabels($from->location, $to->location);
+    }
+
+    /**
+     * @param \stdClass $from
+     * @param \stdClass $to
+     */
+    private function copyOrganizer(\stdClass $from, \stdClass $to)
+    {
+        if (!isset($from->organizer)) {
+            return;
+        }
+
+        if (!isset($to->organizer)) {
+            $to->organizer = new \stdClass();
+        }
+
+        $this->copyIdentifiers($from->organizer, $to->organizer, 'Organizer');
+        $to->organizer->name = $from->organizer->name;
+        $this->copyLabels($from->organizer, $to->organizer);
     }
 }
