0426ad9945751ee726af4a6453e143a2954358ef
Bert Ramakers <bertramakers@me.com>
2017-05-15 14:58:50 +0200

    III-2080: Rename IndexVersions to SchemaVersions

--- src/Operations/SchemaVersions.php
+++ src/Operations/SchemaVersions.php
@@ -2,7 +2,7 @@
 
 namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
-class IndexVersions
+class SchemaVersions
 {
     const UDB3_CORE = 20170515145300;
     const GEOSHAPES = 20170515145300;
--- tests/Operations/SchemaVersionsTest.php
+++ tests/Operations/SchemaVersionsTest.php
@@ -2,14 +2,14 @@
 
 namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
-class IndexVersionsTest extends \PHPUnit_Framework_TestCase
+class SchemaVersionsTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @test
      */
     public function it_has_the_latest_udb3_core_version()
     {
-        $this->assertTrue(defined(IndexVersions::class . '::UDB3_CORE'));
+        $this->assertTrue(defined(SchemaVersions::class . '::UDB3_CORE'));
     }
 
     /**
@@ -17,6 +17,6 @@ class IndexVersionsTest extends \PHPUnit_Framework_TestCase
      */
     public function it_has_the_latest_geoshapes_version()
     {
-        $this->assertTrue(defined(IndexVersions::class . '::GEOSHAPES'));
+        $this->assertTrue(defined(SchemaVersions::class . '::GEOSHAPES'));
     }
 }
