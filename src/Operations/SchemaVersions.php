59afad9b36dc9d2f7a0c6685ebf1dd0cc167acc1
Bert Ramakers <bertramakers@me.com>
2017-06-14 17:41:46 +0200

    III-2162: Update event and place mapping to make address translatable

--- src/Operations/SchemaVersions.php
+++ src/Operations/SchemaVersions.php
@@ -4,6 +4,6 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 class SchemaVersions
 {
-    const UDB3_CORE = 20170609094600;
+    const UDB3_CORE = 20170614165600;
     const GEOSHAPES = 20170515145300;
 }
