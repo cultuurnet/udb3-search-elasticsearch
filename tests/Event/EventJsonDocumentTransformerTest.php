040d28dc98a9531d416ccab1dfef2ed731273708
Bert Ramakers <bertramakers@me.com>
2017-06-15 14:51:07 +0200

    III-2162: Change tests to use new address format and keep a separate test for old format

--- tests/Event/EventJsonDocumentTransformerTest.php
+++ tests/Event/EventJsonDocumentTransformerTest.php
@@ -442,4 +442,20 @@ class EventJsonDocumentTransformerTest extends \PHPUnit_Framework_TestCase
 
         $this->assertJsonDocumentEquals($this, $expectedDocument, $actualDocument);
     }
+
+    /**
+     * @test
+     */
+    public function it_should_transform_addresses_in_a_deprecated_format()
+    {
+        $original = file_get_contents(__DIR__ . '/data/original-with-deprecated-address-format.json');
+        $originalDocument = new JsonDocument('179c89c5-dba4-417b-ae96-62e7a12c2405', $original);
+
+        $expected = file_get_contents(__DIR__ . '/data/indexed.json');
+        $expectedDocument = new JsonDocument('179c89c5-dba4-417b-ae96-62e7a12c2405', $expected);
+
+        $actualDocument = $this->transformer->transform($originalDocument);
+
+        $this->assertJsonDocumentEquals($this, $expectedDocument, $actualDocument);
+    }
 }
