eeea108e6487adbbfd6ff11b60b6b3bd8ed938a4
Luc Wollants <luc.wollants@apptite.be>
2017-06-20 11:07:21 +0200

    III-2163 Factor out AbstractCopyJSONLD from CopyJsonOrganizer.

--- src/Organizer/CopyJsonOrganizer.php
+++ src/Organizer/CopyJsonOrganizer.php
@@ -2,9 +2,52 @@
 
 namespace CultuurNet\UDB3\Search\ElasticSearch\Organizer;
 
-use CultuurNet\UDB3\Search\ElasticSearch\JsonDocument\CopyJson\AbstractCopyJSONLD;
+use CultuurNet\UDB3\Search\ElasticSearch\IdUrlParserInterface;
+use CultuurNet\UDB3\Search\ElasticSearch\JsonDocument\CopyJson\CopyJsonIdentifier;
+use CultuurNet\UDB3\Search\ElasticSearch\JsonDocument\CopyJson\CopyJsonInterface;
+use CultuurNet\UDB3\Search\ElasticSearch\JsonDocument\CopyJson\CopyJsonName;
+use CultuurNet\UDB3\Search\ElasticSearch\JsonDocument\CopyJson\FallbackType;
+use Psr\Log\LoggerInterface;
 
-class CopyJsonOrganizer extends AbstractCopyJSONLD
+class CopyJsonOrganizer implements CopyJsonInterface
 {
+    /**
+     * @var CopyJsonIdentifier
+     */
+    private $copyJsonIdentifier;
 
+    /**
+     * @var CopyJsonName
+     */
+    private $copyJsonName;
+
+    /**
+     * @param LoggerInterface $logger
+     * @param IdUrlParserInterface $idUrlParser
+     * @param FallbackType $fallbackType
+     */
+    public function __construct(
+        LoggerInterface $logger,
+        IdUrlParserInterface $idUrlParser,
+        FallbackType $fallbackType
+    ) {
+        $this->copyJsonIdentifier = new CopyJsonIdentifier(
+            $logger,
+            $idUrlParser,
+            $fallbackType
+        );
+
+        $this->copyJsonName = new CopyJsonName($logger);
+    }
+
+    /**
+     * @param \stdClass $from
+     * @param \stdClass $to
+     */
+    public function copy(\stdClass $from, \stdClass $to)
+    {
+        $this->copyJsonIdentifier->copy($from, $to);
+
+        $this->copyJsonName->copy($from, $to);
+    }
 }
