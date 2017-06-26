d625d7a8fb3b5b140b84e6f37ec11618e1adf766
Bert Ramakers <bertramakers@me.com>
2017-06-26 15:00:59 +0200

    III-2167: Fix code coverage

--- tests/Event/EventJsonDocumentTransformerTest.php
+++ tests/Event/EventJsonDocumentTransformerTest.php
@@ -506,4 +506,20 @@ class EventJsonDocumentTransformerTest extends \PHPUnit_Framework_TestCase
 
         $this->assertJsonDocumentPropertiesEquals($this, $expectedDocument, $actualDocument);
     }
+
+    /**
+     * @test
+     */
+    public function it_should_copy_languages_and_completed_languages_if_present_on_the_json_ld()
+    {
+        $original = file_get_contents(__DIR__ . '/data/original-with-languages.json');
+        $originalDocument = new JsonDocument('23017cb7-e515-47b4-87c4-780735acc942', $original);
+
+        $expected = file_get_contents(__DIR__ . '/data/indexed.json');
+        $expectedDocument = new JsonDocument('23017cb7-e515-47b4-87c4-780735acc942', $expected);
+
+        $actualDocument = $this->transformer->transform($originalDocument);
+
+        $this->assertJsonDocumentPropertiesEquals($this, $expectedDocument, $actualDocument);
+    }
 }
