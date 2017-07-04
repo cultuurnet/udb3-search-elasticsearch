79e3664538a2762bc674c297a3c248c00e22e13b
Bert Ramakers <bertramakers@me.com>
2017-07-04 13:54:10 +0200

    III-2200: Increment the udb3_core schema version number

--- src/Operations/SchemaVersions.php
+++ src/Operations/SchemaVersions.php
@@ -4,6 +4,6 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 class SchemaVersions
 {
-    const UDB3_CORE = 20170614165600;
+    const UDB3_CORE = 20170704135300;
     const GEOSHAPES = 20170515145300;
 }
