c5990411f7aac301441c86b5729c61ca1b2ac398
Kristof Willaert <kristof.willaert@cultuurnet.be>
2017-09-28 11:16:49 +0200

    Up geoshapes schema version, forcing reindex to use NGI geojson data

--- src/Operations/SchemaVersions.php
+++ src/Operations/SchemaVersions.php
@@ -5,5 +5,5 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 class SchemaVersions
 {
     const UDB3_CORE = 20170704135300;
-    const GEOSHAPES = 20170515145300;
+    const GEOSHAPES = 20170928091500;
 }
