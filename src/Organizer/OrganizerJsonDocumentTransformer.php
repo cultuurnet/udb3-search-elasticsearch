482795141c856ba65795d34a55d0e89fa4443225
Luc Wollants <luc.wollants@apptite.be>
2018-04-11 15:22:14 +0200

    III-2509 Index the address (translatable and non-translatable).

--- src/Organizer/OrganizerJsonDocumentTransformer.php
+++ src/Organizer/OrganizerJsonDocumentTransformer.php
@@ -16,12 +16,19 @@ class OrganizerJsonDocumentTransformer implements JsonDocumentTransformerInterfa
      */
     private $jsonCopier;
 
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
     public function __construct(
         IdUrlParserInterface $idUrlParser,
         LoggerInterface $logger
     ) {
+        $this->logger = $logger;
+
         $this->jsonCopier = new CopyJsonOrganizer(
-            new CopyJsonPsrLogger($logger),
+            new CopyJsonPsrLogger($this->logger),
             $idUrlParser
         );
     }
@@ -40,6 +47,59 @@ class OrganizerJsonDocumentTransformer implements JsonDocumentTransformerInterfa
 
         $newBody->url = $body->url;
 
+        $this->copyAddress($body, $newBody);
+
         return $jsonDocument->withBody($newBody);
+    }
+
+    /**
+     * @param \stdClass $from
+     * @param \stdClass $to
+     */
+    protected function copyAddress(\stdClass $from, \stdClass $to)
+    {
+        // Address is not a required field for an organizer.
+        if (isset($from->address)) {
+            $mainLanguage = isset($from->mainLanguage) ? $from->mainLanguage : 'nl';
+
+            if (isset($from->address->streetAddress)) {
+                // Old JSON-LD does not have a multilingual address.
+                $from->address = (object)[$mainLanguage => $from->address];
+            }
+
+            $addressLanguages = array_keys(get_object_vars($from->address));
+            $fields = ['addressCountry', 'addressLocality', 'postalCode', 'streetAddress'];
+            $copiedAddresses = [];
+
+            foreach ($addressLanguages as $addressLanguage) {
+                $address = $from->address->{$addressLanguage};
+                $copiedAddress = [];
+
+                foreach ($fields as $field) {
+                    if (!isset($address->{$field})) {
+                        $this->logMissingExpectedField("address.{$addressLanguage}.{$field}");
+                        continue;
+                    }
+
+                    $copiedAddress[$field] = $address->{$field};
+                }
+
+                if (!empty($copiedAddress)) {
+                    $copiedAddresses[$addressLanguage] = (object)$copiedAddress;
+                }
+            }
+
+            if (!empty($copiedAddresses)) {
+                $to->address = (object)$copiedAddresses;
+            }
+        }
+    }
+
+    /**
+     * @param $fieldName
+     */
+    protected function logMissingExpectedField($fieldName)
+    {
+        $this->logger->warning("Missing expected field '{$fieldName}'.");
     }
 }
--- tests/Organizer/OrganizerJsonDocumentTransformerTest.php
+++ tests/Organizer/OrganizerJsonDocumentTransformerTest.php
@@ -100,4 +100,36 @@ class OrganizerJsonDocumentTransformerTest extends \PHPUnit_Framework_TestCase
 
         $this->assertJsonDocumentPropertiesEquals($this, $expectedDocument, $actualDocument);
     }
+
+    /**
+     * @test
+     */
+    public function it_handles_translated_address()
+    {
+        $original = file_get_contents(__DIR__ . '/data/original_with_translated_address.json');
+        $originalDocument = new JsonDocument('5e0b3f9c-5947-46a0-b8f2-a1a5a37f3b83', $original);
+
+        $expected = file_get_contents(__DIR__ . '/data/indexed_with_translated_address.json');
+        $expectedDocument = new JsonDocument('5e0b3f9c-5947-46a0-b8f2-a1a5a37f3b83', $expected);
+
+        $actualDocument = $this->transformer->transform($originalDocument);
+
+        $this->assertJsonDocumentPropertiesEquals($this, $expectedDocument, $actualDocument);
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_non_translated_address()
+    {
+        $original = file_get_contents(__DIR__ . '/data/original_with_non_translated_address.json');
+        $originalDocument = new JsonDocument('5e0b3f9c-5947-46a0-b8f2-a1a5a37f3b83', $original);
+
+        $expected = file_get_contents(__DIR__ . '/data/indexed_with_non_translated_address.json');
+        $expectedDocument = new JsonDocument('5e0b3f9c-5947-46a0-b8f2-a1a5a37f3b83', $expected);
+
+        $actualDocument = $this->transformer->transform($originalDocument);
+
+        $this->assertJsonDocumentPropertiesEquals($this, $expectedDocument, $actualDocument);
+    }
 }
