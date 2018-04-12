6b5813bfb93f7c0b95a161a34ed5dc62c9a10171
Luc Wollants <luc.wollants@apptite.be>
2018-04-12 13:18:13 +0200

    III-2509 Add postal code as query parameter for organizer.

--- src/Organizer/ElasticSearchOrganizerQueryBuilder.php
+++ src/Organizer/ElasticSearchOrganizerQueryBuilder.php
@@ -2,6 +2,7 @@
 
 namespace CultuurNet\UDB3\Search\ElasticSearch\Organizer;
 
+use CultuurNet\UDB3\Address\PostalCode;
 use CultuurNet\UDB3\Language;
 use CultuurNet\UDB3\Search\ElasticSearch\AbstractElasticSearchQueryBuilder;
 use CultuurNet\UDB3\Search\Organizer\OrganizerQueryBuilderInterface;
@@ -34,5 +35,23 @@ class ElasticSearchOrganizerQueryBuilder extends AbstractElasticSearchQueryBuild
     public function withWebsiteFilter(Url $url)
     {
         return $this->withMatchQuery('url', (string) $url);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function withPostalCodeFilter(PostalCode $postalCode)
+    {
+        // @todo: The list of known languages gets bigger.
+        // @see https://jira.uitdatabank.be/browse/III-2161 (es and it)
+        return $this->withMultiFieldMatchQuery(
+            [
+                'address.nl.postalCode',
+                'address.fr.postalCode',
+                'address.de.postalCode',
+                'address.en.postalCode',
+            ],
+            $postalCode->toNative()
+        );
     }
 }
