06d9612298cd2e824a63f53890cf318057e4f4dd
Reinout Bossuyt <reinout@cultuurnet.be>
2018-07-10 14:30:25 +0200

    III-2619 update schema version

--- src/Operations/SchemaVersions.php
+++ src/Operations/SchemaVersions.php
@@ -4,6 +4,6 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 class SchemaVersions
 {
-    const UDB3_CORE = 20170704135300;
+    const UDB3_CORE = 20180710142700;
     const GEOSHAPES = 20170928091500;
 }
