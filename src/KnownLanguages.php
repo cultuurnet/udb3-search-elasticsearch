4a6ab2d3d3c2217a9756e97bc25506fb4a5c6866
Luc Wollants <luc.wollants@apptite.be>
2018-10-22 08:52:05 +0200

    III-2763 Add typehint for fieldPattern and rename to camel case.

--- src/KnownLanguages.php
+++ src/KnownLanguages.php
@@ -4,15 +4,19 @@ namespace CultuurNet\UDB3\Search\ElasticSearch;
 
 class KnownLanguages
 {
-    public function fieldNames($field_pattern): array
+    /**
+     * @param string $fieldPattern
+     * @return array
+     */
+    public function fieldNames(string $fieldPattern): array
     {
         // @todo: The list of known languages gets bigger.
         // @see https://jira.uitdatabank.be/browse/III-2161 (es and it)
         $knownLanguages = ['nl', 'fr', 'de', 'en'];
 
         return array_map(
-            function ($languageCode) use ($field_pattern) {
-                return str_replace('{{lang}}', $languageCode, $field_pattern);
+            function ($languageCode) use ($fieldPattern) {
+                return str_replace('{{lang}}', $languageCode, $fieldPattern);
             },
             $knownLanguages
         );
