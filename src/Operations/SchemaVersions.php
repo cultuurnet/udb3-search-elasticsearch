52a2d04c00f2dd2cfff134310fb08ff4b7781577
reinoutbossuyt <reinout.bossuyt@cultuurnet.be>
2019-06-19 06:59:57 +0200

    III-2921 update index

--- src/Operations/SchemaVersions.php
+++ src/Operations/SchemaVersions.php
@@ -4,6 +4,6 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 class SchemaVersions
 {
-    const UDB3_CORE = 20181002161700;
+    const UDB3_CORE = 20190618070000;
     const GEOSHAPES = 20190111135400;
 }
